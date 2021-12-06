63baba045d04f9ae63ab129bd6b3184cdb1bf843
Kurilo <reloded2004@hotmail.com>
2021-12-06 14:42:31 -0500

    connected services

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../recipe.model';
+
+import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -7,15 +9,13 @@ import { Recipe } from '../../recipe.model';
   styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent implements OnInit {
-  @Output() recipeSelected = new EventEmitter<void>();
- 
   @Input() recipe: Recipe;
-  constructor() {}
 
-  onSelected() {
-    this.recipeSelected.emit();
-  }
-
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {}
+
+  onSelected() {
+    this.recipeService.recipeSelected.emit(this.recipe);
+  }
 }
