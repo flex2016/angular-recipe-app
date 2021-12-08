3e0dd3eb48a26647aa5371635e7b004dee0dda49
Kurilo <reloded2004@hotmail.com>
2021-12-07 20:58:32 -0500

    connected router

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,21 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Recipe } from '../../recipe.model';
+import { Component, OnInit, Input } from '@angular/core';
 
-import { RecipeService } from '../../recipe.service';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css'],
+  styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Input() index: number;
 
-  constructor(private recipeService: RecipeService) {}
-
-  ngOnInit(): void {}
-
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
+  ngOnInit() {
   }
 }
