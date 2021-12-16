574ae615086f8ea2cafa302f25db6ee525929575
Kurilo <reloded2004@hotmail.com>
2021-12-16 14:05:29 -0500

    updated auth to use ngrx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 
-import { AuthService } from './auth/auth.service';
 import { LoggingService } from './logging.service';
+import * as fromApp from './store/app.reducer';
+import * as AuthActions from './auth/store/auth.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   constructor(
-    private authService: AuthService,
+    private store: Store<fromApp.AppState>,
     private loggingService: LoggingService
   ) {}
 
   ngOnInit() {
-    this.authService.autoLogin();
+    this.store.dispatch(new AuthActions.AutoLogin());
     this.loggingService.printLog('Hello from AppComponent ngOnInit');
   }
 }
